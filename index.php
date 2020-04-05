dfec1717be3022900ca57bdf64a21847895c5ebf
krisonecvel <anisirkonechvel@gmail.com>
2020-04-06 01:29:19 +0300

    second task

--- index.php
+++ index.php
@@ -2,6 +2,49 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Кристина'; // укажите здесь ваше имя
+$category_list = [
+    "boards" => "Доски и Лыжи",
+    "bindings" => "Крепления",
+    "boots" => "Ботинки",
+    "clothes" => "Одежда",
+    "instruments" => "Инструменты",
+    "other" => "Разное",
+];
+
+$name_list =
+    [
+        [
+            "name" => "2014 Rossignol District Snowboard",
+            "category" => "boards",
+            "price" => 10999,
+            "image" => "img/lot-1.jpg"],
+        [
+            "name" => "DC Ply Mens 2016/2017 Snowboard",
+            "category" => "boards",
+            "price" => 159999,
+            "image" => "img/lot-2.jpg",],
+        [
+            "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
+            "category" => "bindings",
+            "price" => 8000,
+            "image" => "img/lot-3.jpg",],
+        [
+            "name" => "Ботинки для сноуборда DC Mutiny Charocal",
+            "category" => "boots",
+            "price" => 10999,
+            "image" => "img/lot-4.jpg",],
+        [
+            "name" => "Куртка для сноуборда DC Mutiny Charocal",
+            "category" => "clothes",
+            "price" => 7500,
+            "image" => "img/lot-5.jpg",],
+        [
+            "name" => "Маска Oakley Canopy",
+            "category" => "other",
+            "price" => 5400,
+            "image" => "img/lot-6.jpg",]
+    ];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
